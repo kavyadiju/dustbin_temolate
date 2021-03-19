3f3b6615122976a8623b074dd2d115924486f12d
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-03-19 14:45:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,10 +13,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	dustbinObj=new dustbin(1150,650);
-	paperObject=new paper(200,450,40);
-	groundObject=new ground(width/2,670,width,20);
-  launcherObj=new Launcher(paperObject.body,{x:200, y:450})
+	
 }
 
 
@@ -24,27 +21,13 @@ function draw() {
 
   background("skyblue");
   Engine.update(engine);
- 
-  paperObject.display();
-  groundObject.display();
-  launcherObj.display();
-  dustbinObj.display();
-}
-function mouseDragged() {
-	
 
-	Matter.Body.setPosition(paperObject.body,{x:mouseX,y:mouseY});
-  
 }
 
-function mouseReleased(){
-    launcherObj.fly();
-}
 function keyPressed(){
     if(keyCode === 32 ){
       
-       Matter.Body.setPosition(paperObject.body,{x:200, y:50});
-       launcherObj.attach(paperObject.body);
+     
     }
 }
 
